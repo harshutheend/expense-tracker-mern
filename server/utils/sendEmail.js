295d65e1c824e485a88820a1fe65ddd99cad8fd3
Harsh Sinha <harshutheend@gmail.com>
2026-07-16 23:49:01 +0530

    api key integration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,30 +1,36 @@
-import nodemailer from "nodemailer";
+import * as brevo from "@getbrevo/brevo";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("BREVO_API_KEY exists:", !!process.env.BREVO_API_KEY);
+console.log("SENDER_EMAIL:", process.env.SENDER_EMAIL);
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const apiInstance = new brevo.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY,
+);
 
 const sendEmail = async (to, subject, html) => {
   try {
-    await transporter.sendMail({
-      from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
+    const email = new brevo.SendSmtpEmail();
 
-    console.log("✅ Email sent successfully");
+    email.sender = {
+      name: "Expense Tracker",
+      email: process.env.SENDER_EMAIL,
+    };
+
+    email.to = [{ email: to }];
+
+    email.subject = subject;
+    email.htmlContent = html;
+
+    const result = await apiInstance.sendTransacEmail(email);
+
+    console.log("✅ Email sent", result.body);
   } catch (error) {
-    console.error("❌ Brevo Error:", error);
+    console.error("❌ Brevo API Error:", error);
     throw error;
   }
 };
