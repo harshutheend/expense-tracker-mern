a964e552d8820a390a14650436defc1204b445cc
Harsh Sinha <harshutheend@gmail.com>
2026-07-16 22:13:19 +0530

    debugging

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
