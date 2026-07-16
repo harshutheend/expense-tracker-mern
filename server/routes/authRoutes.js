e8894eaaf30a0768b061ea8b5612e785dff2172e
Harsh Sinha <harshutheend@gmail.com>
2026-07-16 21:01:15 +0530

    edits in authroutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,15 +2,24 @@ import express from "express";
 import {
   sendOTP,
   verifyOTP,
+  forgotPassword,
+  verifyResetOTP,
+  resetPassword,
   loginUser,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Registration
 router.post("/send-otp", sendOTP);
-
 router.post("/verify-otp", verifyOTP);
 
+// Password Reset
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-reset-otp", verifyResetOTP);
+router.post("/reset-password", resetPassword);
+
+// Login
 router.post("/login", loginUser);
 
 export default router;
